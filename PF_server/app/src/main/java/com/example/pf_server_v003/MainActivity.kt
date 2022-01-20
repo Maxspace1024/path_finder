3a1756eb3a7e8bfca5b7aa1a8bf59777cc36b853
Maxspace1024 <jeb.3332@gmail.com>
2022-01-20 15:59:03 +0800

    加入spinner還有nchumap

--- PF_server/app/src/main/java/com/example/pf_server_v003/MainActivity.kt
+++ PF_server/app/src/main/java/com/example/pf_server_v003/MainActivity.kt
@@ -4,20 +4,20 @@ import android.content.Intent
 import android.os.*
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.lang.Exception
 import java.net.ServerSocket
+import android.widget.AdapterView
+import android.widget.AdapterView.*
 import java.net.Socket
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,OnItemSelectedListener{
     private lateinit var imgArrow : ImageView
     private lateinit var btnStart : Button
     private lateinit var tvMsg : TextView
@@ -26,6 +26,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var serHandler: Handler
     private lateinit var mainHandler: Handler
     private lateinit var serverHT : HandlerThread
+
+    private lateinit var spin001 : Spinner
 
     private lateinit var jData : JSONObject
     private var serverIsRunning : Boolean = false
@@ -38,6 +40,8 @@ class MainActivity : AppCompatActivity() {
         btnStart=findViewById(R.id.btnStart)
         tvMsg=findViewById(R.id.tvMsg)
         tvServerStat = findViewById(R.id.tvServerStat)
+        spin001=findViewById(R.id.spin_loc)
+
 
         serverHT=HandlerThread("serverx")
         serverHT.start()
@@ -47,8 +51,8 @@ class MainActivity : AppCompatActivity() {
             //"it" is a instance of Message()
             when(it.what){
                 MSGtype.SERV_ON.ordinal         -> {
-                    Log.d("serverx", "create a server")
-                    tvServerStat.setText("create a server")
+                    Log.d("serverx", "running a server")
+                    tvServerStat.setText("running a server")
                 }
                 MSGtype.SERV_BREAK_LOOP.ordinal -> {
                     Log.d("serverx", "close a server(break loop)")
@@ -57,6 +61,9 @@ class MainActivity : AppCompatActivity() {
                 }
                 MSGtype.SERV_IMG_ROT.ordinal    -> {
                     Log.d("serverx", jData.toString())
+                    //Log.d("selectt", spin001.selectedItem.toString())
+                    //Toast.makeText(this,spin001.selectedItem.toString(),Toast.LENGTH_SHORT).show()
+                    //==
                     var way = jData.get("Way").toString()
                     when(way){
                         "North" -> imgArrow.rotation=0.0f
@@ -72,6 +79,7 @@ class MainActivity : AppCompatActivity() {
                             }
                         }
                     }
+                    //==
                     tvMsg.setText(jData.toString())
                 }
                 MSGtype.SERV_OFF.ordinal        -> tvServerStat.setText("close a server")
@@ -80,51 +88,67 @@ class MainActivity : AppCompatActivity() {
         })
 
 
+        var adapter : ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
+            this,R.array.loc_array,android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spin001.adapter = adapter
+        spin001.onItemSelectedListener = this
+
+
         btnStart.setOnClickListener {
-            if(serverIsRunning){
+            if (serverIsRunning) {
                 serverIsRunning = false
                 mainHandler.sendMessage(
                     // a anonymous instance initialize using ".also"
                     Message().also { it.what = MSGtype.SERV_BREAK_LOOP.ordinal }
                 )
-            }
-            else {
+            } else {
                 serHandler.post {
-                        try {
-                            val server = ServerSocket(5566)
+                    try {
+                        val server = ServerSocket(5566)
+                        mainHandler.sendMessage(
+                            Message().also { it.what = MSGtype.SERV_ON.ordinal }
+                        )
+
+                        serverIsRunning = true
+                        while (serverIsRunning) {
+                            val client = server.accept()
+                            val output = PrintWriter(client.getOutputStream(), true)
+                            val input = BufferedReader(InputStreamReader(client.getInputStream()))
+
+                            jData = JSONObject(input.readLine())
+                            output.println("server got the jXon str")      //reply
+
                             mainHandler.sendMessage(
-                                Message().also { it.what = MSGtype.SERV_ON.ordinal }
+                                Message().also { it.what = MSGtype.SERV_IMG_ROT.ordinal }
                             )
 
-                            serverIsRunning = true
-                            while (serverIsRunning) {
-                                val client = server.accept()
-                                val output = PrintWriter(client.getOutputStream(), true)
-                                val input = BufferedReader(InputStreamReader(client.getInputStream()))
-
-                                jData = JSONObject(input.readLine())
-                                output.println("server got the jXon str")      //reply
-
-                                mainHandler.sendMessage(
-                                    Message().also { it.what = MSGtype.SERV_IMG_ROT.ordinal }
-                                )
-
-                                input.close()
-                                output.close()
-                            }
-
-                            server.close()
-                            mainHandler.sendMessage(
-                                Message().also { it.what = MSGtype.SERV_OFF.ordinal }
-                            )
-                        } catch (e: Exception) {
-                            Log.d("serverx", e.toString())
-                            this@MainActivity.runOnUiThread {
-                                tvServerStat.setText(e.toString())
-                            }
+                            input.close()
+                            output.close()
                         }
+
+                        server.close()
+                        mainHandler.sendMessage(
+                            Message().also { it.what = MSGtype.SERV_OFF.ordinal }
+                        )
+                    } catch (e: Exception) {
+                        Log.d("serverx", e.toString())
+                        this@MainActivity.runOnUiThread {
+                            tvServerStat.setText(e.toString())
+                        }
+                    }
                 }
             }
+
         }
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        var text : String = p0!!.getItemAtPosition(p2).toString()
+        Toast.makeText(p0.context,text,Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
